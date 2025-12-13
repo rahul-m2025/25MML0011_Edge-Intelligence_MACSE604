5e45bcba6b554f5e40745a69d40a600a785ac149
rahul-m2025 <rahul.m2025@vitstudent.ac.in>
2025-12-13 17:09:46 +0530

    Add files via upload

---
+++ Lab 2/logs.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+df=pd.read_csv("C:/Users/batch1/Downloads/logs_dataset.csv")
+
+
+# In[10]:
+
+
+print(df)
+
+
+# In[5]:
+
+
+print(df.head())
+
+
+# In[7]:
+
+
+print(len(df))
+
+
+# In[8]:
+
+
+print(df.shape[0])
+
+
+# In[9]:
+
+
+print(len(df.index))
+
+
+# In[ ]:
+
+
+
+
